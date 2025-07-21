938fa0bdba1b8d3fa407dbf5f1b03e8706911476
SunshineMoonGit <sunshinemoongit@gmail.com>
2025-07-21 23:05:19 +0900

    [feature#1] 로그인 페이지 첫번째 구현

--- app/api/auth/login/page.tsx
+++ app/api/auth/login/page.tsx
@@ -1,14 +1,81 @@
-"use client";
+import Image from "next/image";
+import styles from "./page.module.scss";
 
-import styles from "./page.module.css";
-import { signIn } from "next-auth/react";
-
-export default function LoginPage() {
+export default function Page() {
   return (
     <div className={styles.container}>
-      123
-      {/* <h1>로그인</h1>
-      <button onClick={() => signIn("github")}>GitHub로 로그인</button> */}
+      <div className={styles.card}>
+        {/* left */}
+        <div className={styles.left}>
+          <h1 className={styles.logo}>Reciplay</h1>
+          <p className={styles.tagline}>자유롭게 배우는 우리</p>
+
+          <form className={styles.form}>
+            <input type="email" placeholder="이메일" className={styles.input} />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              className={styles.input}
+            />
+            <button type="submit" className={styles.button}>
+              로그인
+            </button>
+          </form>
+
+          <div className={styles.links}>
+            <a href="#">아이디 찾기</a>
+            <span> | </span>
+            <a href="#">비밀번호 찾기</a>
+            <span> | </span>
+            <a href="#">회원가입</a>
+          </div>
+
+          <div className={styles.separator}>
+            <span className={styles.line} />
+            <span className={styles.or}>or</span>
+            <span className={styles.line} />
+          </div>
+          <p className={styles.tagline}>SNS 계정으로 간편 로그인</p>
+          <div className={styles.snsIcons}>
+            <a href="#">
+              <Image
+                src="/icons/apple.svg"
+                alt="Apple"
+                width={28}
+                height={28}
+              />
+            </a>
+            <a href="#">
+              <Image
+                src="/icons/google.svg"
+                alt="Google"
+                width={28}
+                height={28}
+              />
+            </a>
+            <a href="#">
+              <Image
+                src="/icons/kakao.svg"
+                alt="Kakao"
+                width={28}
+                height={28}
+              />
+            </a>
+          </div>
+        </div>
+
+        {/* right */}
+        <div className={styles.right}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/images/auth-image.jpg"
+              alt="Reciplay"
+              fill
+              className={styles.image}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
