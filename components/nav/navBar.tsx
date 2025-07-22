992ffe184c71c38d792508f53bf23f092ddffba7
강태욱 <sunshinemoongit@gmail.com>
2025-07-22 12:47:03 +0900

    [feature#3] 네비게이션 바 생성

---
+++ components/nav/navBar.tsx
@@ -0,0 +1,32 @@
+import BaseInput from "@/components/input/baseInput";
+import styles from "./navBar.module.scss";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Image
+          src="/icons/hamburgerBtn.svg"
+          alt="hamberger"
+          width={24}
+          height={24}
+        />
+        <div className={styles.logo}>Reciplay</div>
+      </div>
+
+      <div>
+        <BaseInput type="search" placeholder="검색" />
+      </div>
+      <div className={styles.right}>
+        <Image
+          className={styles.profile}
+          src="/images/profile.png"
+          alt="profile"
+          width={40}
+          height={40}
+        />
+      </div>
+    </div>
+  );
+}
