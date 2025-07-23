26ac64f858e9fe58f54bcc1bb51af3919c3cff78
leewonjun <evenil0206@gmail.com>
2025-07-23 12:33:58 +0900

    [feat#2] 회원가입, Swagger처리 S13P11E104-53 /practice-ui.html 로 swagger 페이지 접속 할 수 있음. 회원 가입 단위 테스트 코드도 작성함.

--- src/main/java/com/e104/reciplay/security/config/SecurityConfig.java
+++ src/main/java/com/e104/reciplay/security/config/SecurityConfig.java
@@ -1,20 +1,50 @@
 package com.e104.reciplay.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${application.url-prefix}")
+    private String URL_PREFIX;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(auth -> auth.disable());
+
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+
         http.authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll());
+                .requestMatchers(URL_PREFIX + "/login",
+                                 URL_PREFIX + "/signup",
+                                 "/h2-console/**",
+                                 "/practice-ui.html",
+                                 "/swagger-ui/**", // swagger-ui 관련 모든 경로 허용
+                                 "/api-docs/json/**",  // openapi v3 문서 경로 허용
+                                 "/swagger-resources/**").permitAll()
+                .requestMatchers(URL_PREFIX + "/admin").hasRole("ADMIN")
+                .anyRequest().authenticated());
+
+        http.formLogin(auth -> auth.disable());
+        // 커스텀 필터를 addFilterAt 해줘야 함.
+
+
+        // 가장 중요한 처리 : 세션을 생성하지 않도록 함.
+        http.sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 }
---
+++ src/test/java/com/e104/reciplay/security/service/SignupServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.e104.reciplay.security.service;
+
+import com.e104.reciplay.security.dto.SignupRequest;
+import com.e104.reciplay.security.exception.DuplicateUserEmailException;
+import com.e104.reciplay.security.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SignupServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @InjectMocks
+    private SignupServiceImpl signupService;
+
+    private static final String TEST_USER_MAIL = "example@mail.com";
+    private static final String TEST_USER_PASSWORD = "123";
+
+    @Test
+    public void 회원가입에_성공한다() {
+        Mockito.when(userRepository.existsByEmail(TEST_USER_MAIL)).thenReturn(false);
+        Mockito.when(bCryptPasswordEncoder.encode(TEST_USER_PASSWORD)).thenReturn(TEST_USER_PASSWORD);
+
+        SignupRequest request = new SignupRequest(TEST_USER_MAIL, TEST_USER_PASSWORD, "");
+
+        assertDoesNotThrow(()->signupService.signup(request));
+        Mockito.verify(bCryptPasswordEncoder, Mockito.times(1)).encode(TEST_USER_PASSWORD);
+    }
+
+    @Test
+    public void 회원가입에_실패한다() {
+        Mockito.when(userRepository.existsByEmail(TEST_USER_MAIL)).thenReturn(true);
+
+        SignupRequest request = new SignupRequest(TEST_USER_MAIL, TEST_USER_PASSWORD, "");
+
+        assertThrows(DuplicateUserEmailException.class, () -> signupService.signup(request));
+    }
+}
