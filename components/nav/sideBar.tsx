b4a90776d24c9aa1c1c2a6565f7bc739ff70ef00
강태욱 <sunshinemoongit@gmail.com>
2025-07-24 17:37:14 +0900

    [feature#25] 네비게이션 바 라우팅 수정 중

--- components/nav/sideBar.tsx
+++ components/nav/sideBar.tsx
@@ -1,27 +1,48 @@
 import Link from "next/link";
 import IconWithText from "../text/iconWithText";
 import styles from "./sideBar.module.scss";
+import { ROUTES } from "@/config/routes";
 
 export default function SideBar() {
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.section}>
+      <div className={styles.sectionList}>
         <IconWithText iconName="live" title="전체 라이브" />
         <IconWithText iconName="category" title="카테고리" />
       </div>
 
       <div className={styles.section}>
-        <IconWithText iconName="live" title="내 프로필" />
-        <IconWithText iconName="subscribe" title="구독" />
-        <IconWithText iconName="record" title="기록" />
-        <IconWithText iconName="security" title="보안" />
-        <IconWithText iconName="setting" title="설정" />
+        <div className={styles.sectionTitle}>
+          <Link href={ROUTES.PROFILE}>
+            <IconWithText iconName="arrow" title="내 프로필" left={false} />
+          </Link>
+        </div>
+        <div className={styles.sectionList}>
+          <Link href={ROUTES.SUBSCRIBE}>
+            <IconWithText iconName="subscribe" title="구독" />
+          </Link>
+
+          {/* <Link href={ROUTES.SUBSCRIBE}></Link> */}
+          <IconWithText iconName="record" title="기록" />
+
+          {/* <Link href={ROUTES.SUBSCRIBE}></Link> */}
+          <IconWithText iconName="security" title="보안" />
+
+          {/* <Link href={ROUTES.SUBSCRIBE}></Link> */}
+          <IconWithText iconName="setting" title="설정" />
+        </div>
       </div>
 
       <div className={styles.section}>
-        <IconWithText iconName="list" title="강사 전용" />
-        <IconWithText iconName="list" title="강좌 관리" />
-        <IconWithText iconName="list" title="강좌 생성" />
+        <div className={styles.sectionTitle}>
+          {/* <Link href={ROUTES.SUBSCRIBE}></Link> */}
+          <IconWithText iconName="arrow" title="스튜디오" left={false} />
+        </div>
+        <div className={styles.sectionList}>
+          <IconWithText iconName="list" title="강사 전용" />
+          <IconWithText iconName="list" title="강좌 관리" />
+          <IconWithText iconName="list" title="강좌 생성" />
+        </div>
       </div>
     </aside>
   );
--- components/text/iconWithText.tsx
+++ components/text/iconWithText.tsx
@@ -4,19 +4,41 @@ import styles from "./iconWithText.module.scss";
 interface IconWithTextProps {
   iconName: string;
   title: string;
+  left?: boolean;
+  size?: number;
   onClick?: () => void;
 }
 
-export default function IconWithText({ iconName, title, onClick }: IconWithTextProps) {
+export default function IconWithText({
+  iconName,
+  title,
+  left = true,
+  size = 20,
+  onClick,
+}: IconWithTextProps) {
+  if (left) {
+    return (
+      <div className={styles.container} onClick={onClick}>
+        <Image
+          src={`/icons/${iconName}.svg`}
+          alt={iconName}
+          width={size}
+          height={size}
+        />
+        <div>{title}</div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container} onClick={onClick}>
+      <div>{title}</div>
       <Image
         src={`/icons/${iconName}.svg`}
         alt={iconName}
-        width={20}
-        height={20}
+        width={size}
+        height={size}
       />
-      <div>{title}</div>
     </div>
   );
 }
