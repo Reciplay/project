8662e07527981ae23b172b9fcf3ec5ad89871dcf
leewonjun <evenil0206@gmail.com>
2025-08-13 10:24:20 +0900

    [fix#4] socket 서버가 Redis기반 인증을 수행하도록 수정함.

--- WebsocketServer/ReciplayWebsocket/src/main/java/com/e104_2/reciplaywebsocket/security/repository/TokenRepository.java
+++ WebsocketServer/ReciplayWebsocket/src/main/java/com/e104_2/reciplaywebsocket/security/repository/TokenRepository.java
@@ -3,6 +3,7 @@ package com.e104_2.reciplaywebsocket.security.repository;
 import com.e104_2.reciplaywebsocket.security.domain.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TokenRepository extends JpaRepository<Token, Long>, CustomTokenRepository {
+import java.util.List;
 
+public interface TokenRepository extends CustomTokenRepository {
 }
