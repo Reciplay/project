c1ae5eeb6ac0b3e1c66bab5a518eeb0dc26cfaf7
leewonjun <evenil0206@gmail.com>
2025-08-15 18:29:03 +0900

    [feature#1] 강좌 반려시 관련 데이터 삭제 구현함. S13P11E104-258

--- Backend/src/main/java/com/e104/reciplay/course/courses/repository/CanLearnRepository.java
+++ Backend/src/main/java/com/e104/reciplay/course/courses/repository/CanLearnRepository.java
@@ -2,9 +2,15 @@ package com.e104.reciplay.course.courses.repository;
 
 import com.e104.reciplay.course.courses.repository.custom.CustomCanLearnRepository;
 import com.e104.reciplay.entity.CanLearn;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface CanLearnRepository extends JpaRepository<CanLearn, Long>, CustomCanLearnRepository {
-    void deleteAllByCourseId(Long courseId);
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Transactional
+    @Query(value = "DELETE FROM can_learn WHERE course_id = :courseId", nativeQuery = true)
+    int deleteAllByCourseId(Long courseId);
 }
