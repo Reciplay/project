e0ab19ac41f081934a4c4c3fddc22bda4f752bd8
DESKTOP-RBNO6LS\SSAFY <jiuk202@gmail.com>
2025-08-15 15:39:32 +0900

    [fix#many08150955] 파일 관련 코드 예외 처리 S13P11E104-254 1. instructorQueryServiceImpl에서 구독자 리스트 조회 변경 2. CourseManagementServiceImpl 강좌 업데이트 파일 관련 try-catch 3. CourseQueryServiceImpl	collectCourseDetailWithCommonFields try 묶인거 따로 설정

---
+++ Backend/src/main/java/com/e104/reciplay/course/lecture/service/ChapterQueryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.e104.reciplay.course.lecture.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChapterQueryServiceImpl implements ChapterQueryService{
+}
