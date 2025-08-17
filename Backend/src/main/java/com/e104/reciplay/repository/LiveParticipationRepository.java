aa59589615c13681102f0d9a7e358e6febb2e01e
leewonjun <evenil0206@gmail.com>
2025-08-17 15:49:06 +0900

    fix 강좌에 새로고침하여 접근할 경우에 튕기는 현상을 굄

--- Backend/src/main/java/com/e104/reciplay/repository/LiveParticipationRepository.java
+++ Backend/src/main/java/com/e104/reciplay/repository/LiveParticipationRepository.java
@@ -4,10 +4,12 @@ import com.e104.reciplay.entity.LiveParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LiveParticipationRepository extends JpaRepository<LiveParticipation, Long> {
     boolean existsByEmail(String email);
     void deleteByLiveRoomId(Long liveRoomId);
     List<LiveParticipation> findByLiveRoomId(Long liveRoomId);
+    Optional<LiveParticipation> findByLiveRoomIdAndEmail(Long liveRoomId, String email);
 }
